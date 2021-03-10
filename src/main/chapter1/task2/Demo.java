3fac909a2d52a5a32fb05370d03c3fa43540bd29
pythonFCG <5120009+pythonFCGa@user.noreply.gitee.com>
2021-03-10 20:20:24 +0800

    Demo

--- src/main/chapter1/task2/Demo.java
+++ src/main/chapter1/task2/Demo.java
@@ -1,4 +1,15 @@
 package main.chapter1.task2;
 
 public class Demo {
+    public static class MyThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("MyThread");
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread myThread = new MyThread();
+        myThread.start();
+    }
 }
